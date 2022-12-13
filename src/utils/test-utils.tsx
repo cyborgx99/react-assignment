bcdee78c0bd678a4c2006bd283b4e30c9dfac973
Serhii Kalashnykov <kalashnykov9@gmail.com>
2022-12-13 23:59:30 +0100

    Add test utils

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { myTheme } from 'common/styles/theme';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'common/styles/globalStyles';
+import { BrowserRouter } from 'react-router-dom';
+import ReactQueryProvider from 'queryProvider';
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider theme={myTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
+
+const customRender = (ui: React.ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };

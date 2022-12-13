99fad08a99c118df8527b3c876c5be88fd12d905
Serhii Kalashnykov <kalashnykov9@gmail.com>
2022-12-14 00:01:57 +0100

    Add pagination stories

---
+++ src/components/pagination/Pagination.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Pagination from './index';
+
+export default {
+  title: 'Components/Pagination',
+  component: Pagination,
+  argTypes: {},
+} as ComponentMeta<typeof Pagination>;
+
+const Template: ComponentStory<typeof Pagination> = (args) => <Pagination {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  currentPage: 1,
+  totalCount: 100,
+  itemsPerPage: 10,
+  onPageChange: (d: number) => {
+    console.log(d);
+  },
+};

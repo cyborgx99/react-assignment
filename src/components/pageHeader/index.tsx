378172bdd1cd7655858094c6d408f1711de8129d
Serhii Kalashnykov <kalashnykov9@gmail.com>
2022-12-16 17:28:08 +0100

    Add navigation controls to header

--- src/components/pageHeader/index.tsx
+++ src/components/pageHeader/index.tsx
@@ -1,22 +1,49 @@
-import React from 'react';
+import NavigationBar from 'components/navigation';
+import React, { useState } from 'react';
 import Icon from '../icon';
-import { headerIconAdditionalStyles, HeaderSpan, HeaderTitle, StyledHeader } from './styles';
+import {
+  headerIconAdditionalStyles,
+  HeaderSpan,
+  HeaderTitle,
+  NavButton,
+  StyledHeader,
+  NavigationContainer,
+} from './styles';
 
 const PageHeader = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsNavbarOpen((previous) => !previous);
+  };
+
   return (
-    <StyledHeader>
-      <Icon
-        title='Home'
-        additionalStyles={headerIconAdditionalStyles}
-        color='text.white'
-        fontSize={2}
-        name='lunch_dining'
-      />
-      <HeaderTitle $color='text.white'>
-        Food
-        <HeaderSpan $color='secondary.100'>Hub</HeaderSpan>
-      </HeaderTitle>
-    </StyledHeader>
+    <>
+      <StyledHeader>
+        <Icon
+          title='Home'
+          additionalStyles={headerIconAdditionalStyles}
+          color='text.white'
+          fontSize={2}
+          name='lunch_dining'
+        />
+        <HeaderTitle $color='text.white'>
+          Food
+          <HeaderSpan $color='secondary.100'>Hub</HeaderSpan>
+        </HeaderTitle>
+        <NavButton onClick={toggleMenu}>
+          <Icon
+            title='Menu'
+            color='text.white'
+            fontSize={2}
+            name={isNavbarOpen ? 'menu_open' : 'menu'}
+          />
+        </NavButton>
+      </StyledHeader>
+      <NavigationContainer $isShown={isNavbarOpen}>
+        <NavigationBar navType='side' />
+      </NavigationContainer>
+    </>
   );
 };
 

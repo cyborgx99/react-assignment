41e1c4a24ba9a99c1dd926c5c0e96ccdb1336e1f
Serhii Kalashnykov <kalashnykov9@gmail.com>
2022-12-31 18:41:20 +0100

    Add pages tests

---
+++ src/components/errorBoundary/ErrorBoundary.test.tsx
@@ -0,0 +1,32 @@
+import { cleanup, render } from 'utils/test-utils';
+import ErrorBoundary from '.';
+
+beforeEach(cleanup);
+
+const ErrorComponent = () => {
+  throw new Error('Help');
+};
+
+describe('Error Boundary', () => {
+  it('renders children if no error is present', () => {
+    const { getByText } = render(<ErrorBoundary>children</ErrorBoundary>);
+    expect(getByText('children')).toBeInTheDocument();
+  });
+
+  it('renders the error message if an error is thrown', () => {
+    console.error = jest.fn();
+    const { getByText, queryByText, getByRole } = render(
+      <ErrorBoundary>
+        children
+        <ErrorComponent />
+      </ErrorBoundary>,
+    );
+    expect(console.error).toHaveBeenCalled();
+
+    expect(queryByText('children')).toBeNull();
+
+    getByText('Something went wrong');
+
+    getByRole('button', { name: 'Reload' });
+  });
+});
---
+++ src/pages/currentOrderPage/CurrentOrderPage.test.tsx
@@ -0,0 +1,52 @@
+import CurrentOrderForm from './currentOrderForm';
+import userEvent from '@testing-library/user-event';
+import { act, cleanup, screen, waitFor } from '@testing-library/react';
+import { render } from 'utils/test-utils';
+
+const formData = {
+  name: 'John',
+  lastName: 'Dee',
+  email: 'john.dee@someemail.com',
+  phone: '123123',
+  address: 'Legnicka 48k',
+  city: 'Wroclaw',
+};
+
+afterEach(cleanup);
+
+test('rendering and submitting a basic Formik form', async () => {
+  const handleSubmit = jest.fn();
+  const { getByLabelText, getByRole } = render(
+    <CurrentOrderForm onHandleOrder={handleSubmit} isLoading={false} error={{ message: '' }} />,
+  );
+  const user = userEvent.setup();
+
+  await user.type(getByLabelText('name'), formData.name);
+  await user.type(getByLabelText('lastName'), formData.lastName);
+  await user.type(getByLabelText('email'), formData.email);
+  await user.type(getByLabelText('phone'), formData.phone);
+  await user.type(getByLabelText('address'), formData.address);
+  await user.type(getByLabelText('city'), formData.city);
+
+  await user.click(getByRole('button', { name: /Order/i }));
+
+  waitFor(() => expect(handleSubmit).toHaveBeenCalledTimes(1));
+});
+
+test('shows field is required errors', async () => {
+  const handleSubmit = jest.fn();
+  const { getAllByText, getByRole } = render(
+    <CurrentOrderForm onHandleOrder={handleSubmit} isLoading={false} error={{ message: '' }} />,
+  );
+  const user = userEvent.setup();
+
+  await act(async () => {
+    await user.click(getByRole('button', { name: /Order/i }));
+  });
+
+  screen.debug();
+
+  await waitFor(() => expect(handleSubmit).toHaveBeenCalledTimes(0));
+
+  expect(getAllByText('Field is required')).toHaveLength(6);
+});

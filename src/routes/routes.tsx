873d92fea9297dad596b117929b356aabd9b4e80
Serhii Kalashnykov <kalashnykov9@gmail.com>
2022-12-31 18:38:22 +0100

    Add not found page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,29 +1,29 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import PageLayout from 'components/layout';
 import Spinner from 'components/spinner';
 import { baseRoutes } from './baseRoutes';
 import { basePathKeys } from './pathKeys';
+import NotFoundPage from 'pages/notFound';
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Suspense
-        fallback={
-          <PageLayout>
-            <Spinner type='animated' />
-          </PageLayout>
-        }
-      >
-        <Routes>
-          <Route path={basePathKeys.HOME} element={<PageLayout />}>
-            {baseRoutes.map(({ path, Component }, index) => (
-              <Route path={path} key={path} index={index === 0} element={<Component />} />
-            ))}
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <Suspense
+      fallback={
+        <PageLayout>
+          <Spinner type='animated' />
+        </PageLayout>
+      }
+    >
+      <Routes>
+        <Route path='*' element={<NotFoundPage />} />
+        <Route path={basePathKeys.HOME} element={<PageLayout />}>
+          {baseRoutes.map(({ path, Component }, index) => (
+            <Route path={path} key={path} index={index === 0} element={<Component />} />
+          ))}
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
